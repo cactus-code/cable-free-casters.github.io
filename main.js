f9c80a0fad360e1eb7a91785c525a0cabe37d453
cactus-code <cactus-code@users.noreply.github.com>
2017-03-30 09:50:12 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    $('.sidebar_title').mouseenter(function() {
-        $('.sidebar_title').fadeTo('fast' , 0.5)
-    });
-    $('.sidebar_title').mouseleave(function() {
-        $('.sidebar_title').fadeTo('fast' , 1)
-    });
-});
-
-$(document).ready(function() {
     $('.sidebar_button').mouseenter(function() {
         $('.sidebar_button').fadeTo('fast' , 0.5)
     });
