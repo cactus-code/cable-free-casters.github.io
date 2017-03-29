902ad551102a9d3bc2a5e0a279932ca9ade306ad
cactus-code <cactus-code@users.noreply.github.com>
2017-03-30 09:49:22 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
+    $('.sidebar_title').mouseenter(function() {
+        $('.sidebar_title').fadeTo('fast' , 0.5)
+    });
+    $('.sidebar_title').mouseleave(function() {
+        $('.sidebar_title').fadeTo('fast' , 1)
+    });
+});
 
+$(document).ready(function() {
+    $('.sidebar_button').mouseenter(function() {
+        $('.sidebar_button').fadeTo('fast' , 0.5)
+    });
+    $('.sidebar_button').mouseleave(function() {
+        $('.sidebar_button').fadeTo('fast' , 1)
+    });
+});
